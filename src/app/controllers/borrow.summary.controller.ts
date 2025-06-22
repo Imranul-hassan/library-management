eecbaba6e85cad147a1cb4b1f55d836c7c0960b2
Akib <imranulhassan3296@gmail.com>
2025-06-22 22:59:05 +0600

    Added borrow summary

---
+++ src/app/controllers/borrow.summary.controller.ts
@@ -0,0 +1,51 @@
+
+import { Borrow } from "../models/borrow.model";
+import { Request, Response } from "express";
+
+export const borrowSummary = async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        }
+      },
+      {
+        $lookup: {
+          from: "books",           
+          localField: "book",
+          foreignField: "_id",
+          as: "bookInfo"
+        }
+      },
+      {
+        $unwind: "$bookInfo"
+      },
+      {
+        $project: {
+          _id: 0,
+          totalQuantity: 1,
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn"
+          }
+        }
+      }
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary
+    });
+
+  } catch (error) {
+    console.error("Aggregation error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while retrieving summary",
+      error
+    });
+  }
+});
--- src/app/routes/borrow.routes.ts
+++ src/app/routes/borrow.routes.ts
@@ -6,6 +6,7 @@ import { borrowBook } from "../controllers/borrow.controller";
 export const borrowRoutes = express.Router();
 
 borrowRoutes.post("/", borrowBook);
+borrowRoutes.get("/", borrowBook);
 
 
 
